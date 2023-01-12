8ece7a36832161eee116b47c8cb3bea26f52380c
Pterodactylae <leonie.schwitzkowski@dci-student.de>
2023-01-12 19:45:22 +0100

    added sound to homepage

--- ToDO.md
+++ ToDO.md
@@ -1,2 +1,10 @@
-# check length of array to not render anymore qwhen theres no data left
 
+
+# overall
+-sound overall 
+
+# quiz: check length of array to not render anymore qwhen theres no data left
+
+
+# Homepage: 
+- do not display navbar on homepage ?
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
+import useSound from 'use-sound'
+import Radio_Waves_Earth_Magnetosphere from '../../src/assets/sounds/Radio_Waves_Earth_Magnetosphere.mp3'
 
 function HomePage() {
+  const soundUrl = '../../src/assets/sounds/Radio_Waves_Earth_Magnetosphere.mp3'
+  const [play, { stop }] = useSound(
+    soundUrl,
+    { volume: 0.5 }
+  );
+  const [isHovering, setIsHovering] =useState(
+    false
+  );
+
   return (
 
-       <div className="Homepage">
-      <h1 data-text="Enter the Quiz Universe" className="home_animation">
+       <div className="Homepage" onMouseEnter={() => {
+        setIsHovering(true);
+        play();
+      }}
+      onMouseLeave={() => {
+        setIsHovering(false);
+        stop();
+      }}>
+      <h1 data-text="Enter the Quiz Universe" className="home_animation" 
+      >
        
        <span>Enter the Quiz Universe</span>
      
@@ -14,37 +33,3 @@ function HomePage() {
 }
 
 export default HomePage
-
-// function Demo() {
-//   // For fun, try swapping out 'rising-pops' with:
-//   // - fanfare
-//   // - dun-dun-dun
-//   // - guitar-loop
-//   const soundUrl = '/sounds/rising-pops.mp3';
-
-//   const [play, { stop }] = useSound(
-//     soundUrl,
-//     { volume: 0.5 }
-//   );
-
-//   const [isHovering, setIsHovering] = React.useState(
-//     false
-//   );
-
-//   return (
-//     <Button
-//       onMouseEnter={() => {
-//         setIsHovering(true);
-//         play();
-//       }}
-//       onMouseLeave={() => {
-//         setIsHovering(false);
-//         stop();
-//       }}
-//     >
-//       <ButtonContents isHovering={isHovering}>
-//         Hover over me!
-//       </ButtonContents>
-//     </Button>
-//   );
-// }

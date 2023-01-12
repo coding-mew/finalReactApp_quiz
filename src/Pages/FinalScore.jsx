151ff578ba87677afe57998a483a219a3057ca3f
Pterodactylae <leonie.schwitzkowski@dci-student.de>
2023-01-12 13:09:21 +0100

    test

--- src/Pages/FinalScore.jsx
+++ src/Pages/FinalScore.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useGameContext } from "../global/Context";
 
 function FinalScore() {
+  console.log("abc")
   const { gameData } = useGameContext()
   return <div className="card">{console.log(gameData.score)}</div>;
 }
